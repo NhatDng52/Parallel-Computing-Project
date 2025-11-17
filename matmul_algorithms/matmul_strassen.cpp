c7da2b67b6dc63e6b33ddefd546484b342ce663d
Dang Tran Minh Nhat <supernhat256@gmail.com>
2025-11-17 22:07:25 +0700

    merege naive algorithm on nhat branch

--- matmul_algorithms/matmul_strassen.cpp
+++ matmul_algorithms/matmul_strassen.cpp
@@ -31,10 +31,10 @@ vector<vector<double>> matrix_mult_strassen(const vector<vector<double>>& A, con
 }
 
 vector<vector<double>> matrix_mult_strassen_OpenMP(const vector<vector<double>>& A, const vector<vector<double>>& B) {
-    // IStrassenOp * strassenOpenMP = new StrassenOpenMPI();
-    // vector<vector<double>> result = strassenOpenMP->apply_strassen(A, B);
-    // delete strassenOpenMP;
-    // return result;
+    IStrassenOp * strassenOpenMP = new StrassenOpenMPI();
+    vector<vector<double>> result = strassenOpenMP->apply_strassen(A, B);
+    delete strassenOpenMP;
+    return result;
 }
 
 vector<vector<double>> matrix_mult_strassen_OpenMPI(const vector<vector<double>>& A, const vector<vector<double>>& B) {
