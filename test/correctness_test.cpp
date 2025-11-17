ba1b310daf8f67a9cedf37c0875eaaba397125e2
Dang Tran Minh Nhat <supernhat256@gmail.com>
2025-11-17 15:44:07 +0700

    implement test

--- test/correctness_test.cpp
+++ test/correctness_test.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 void test_correctness(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<double>>&, const vector<std::vector<double>>&)) {
     test_1(matrix_mult);
-    // test_2(matrix_mult);
-    // test_3(matrix_mult);
+    test_2(matrix_mult);
+    test_3(matrix_mult);
     cout<<"Correctness tests completed \n";
 }
 
@@ -38,9 +38,9 @@ void test_1(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<
 }
 void test_2(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<double>>&, const vector<std::vector<double>>&)) {
     // Small non-square matrix test
-    vector<vector<double>> A = read_matrix_from_csv("./test/test_case/matrix_A_3x4.csv");
-    vector<vector<double>> B = read_matrix_from_csv("./test/test_case/matrix_B_4x2.csv");
-    vector<vector<double>> expected = read_matrix_from_csv("./test/test_case/correctness_2.csv");
+    vector<vector<double>> A = read_matrix_from_csv("./test/test_case/matrix_3x4.csv");
+    vector<vector<double>> B = read_matrix_from_csv("./test/test_case/matrix_4x2.csv");
+    vector<vector<double>> expected = read_matrix_from_csv("./test/test_case/result_correctness_2.csv");
     try
     {
         vector<vector<double>> result = matrix_mult(A, B);
@@ -62,9 +62,9 @@ void test_2(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<
 }   
 void test_3(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<double>>&, const vector<std::vector<double>>&)) {
     //3B 
-    vector<vector<double>> A = read_matrix_from_csv("./test/test_case/matrix_A_256x256.csv");
-    vector<vector<double>> B = read_matrix_from_csv("./test/test_case/matrix_B_256x256.csv");
-    vector<vector<double>> C = read_matrix_from_csv("./test/test_case/matrix_C_256x256.csv");
+    vector<vector<double>> A = read_matrix_from_csv("./test/test_case/medium_random_matrix_A_256x256.csv");
+    vector<vector<double>> B = read_matrix_from_csv("./test/test_case/medium_random_matrix_B_256x256.csv");
+    vector<vector<double>> C = read_matrix_from_csv("./test/test_case/medium_random_matrix_C_256x256.csv");
     try
     {
         vector<vector<double>> AB = matrix_mult(A, B);
@@ -74,6 +74,8 @@ void test_3(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<
         
 
         if(!compare_matrices(ABC_1, ABC_2)) {
+            write_matrix_to_csv("./test/results/3B_ABC_1.csv", ABC_1);
+            write_matrix_to_csv("./test/results/3B_ABC_2.csv", ABC_2);
             cout << "Test 3 Failed: (A * B) * C != A * (B * C)" << endl;
         }
     }
@@ -83,16 +85,18 @@ void test_3(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<
     }
 
     //3E 
-    vector<vector<double>> I = read_matrix_from_csv("./test/test_case/identity_matrix_256x256.csv");
+    vector<vector<double>> I = read_matrix_from_csv("./test/test_case/medium_identity_matrix_256x256.csv");
     vector<vector<double>> AI = matrix_mult(A, I);
     vector<vector<double>> IA = matrix_mult(I, A);
         
     try
     {
         if(!compare_matrices(AI, A)) {
+            write_matrix_to_csv("./test/results/3E_AI.csv", AI);
             cout << "Test 3 Failed: A * I != A" << endl;
         }
         if(!compare_matrices(IA, A)) {
+            write_matrix_to_csv("./test/results/3E_IA.csv", IA);
             cout << "Test 3 Failed: I * A != A" << endl;
         }
     }
@@ -102,15 +106,17 @@ void test_3(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<
     }
 
     //3F 
-    vector<vector<double>> zero = read_matrix_from_csv("./test/test_case/zero_matrix_256x256.csv");
+    vector<vector<double>> zero = read_matrix_from_csv("./test/test_case/medium_zero_matrix_256x256.csv");
     vector<vector<double>> Azero = matrix_mult(A, zero);
     vector<vector<double>> zeroA = matrix_mult(zero, A);
     try
     {
         if(!compare_matrices(Azero, zero)) {
+            write_matrix_to_csv("./test/results/3F_Azero.csv", Azero);
             cout << "Test 3 Failed: A * 0 != 0" << endl;
         }
         if(!compare_matrices(zeroA, zero)) {
+            write_matrix_to_csv("./test/results/3F_zeroA.csv", zeroA);
             cout << "Test 3 Failed: 0 * A != 0" << endl;
         }
     }
@@ -125,6 +131,8 @@ void test_3(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<
     try
     {
         if(compare_matrices(AB, BA)) {
+            write_matrix_to_csv("./test/results/3G_AB.csv", AB);
+            write_matrix_to_csv("./test/results/3G_BA.csv", BA);
             cout << "Test 3 Failed: A * B == B * A" << endl;
         }
     }
--- test/performance_test.cpp
+++ test/performance_test.cpp
@@ -2,11 +2,33 @@
 
 void test_performance(vector<std::vector<double>> (*matrix_mult)(const vector<std::vector<double>>&, const vector<std::vector<double>>&)) {
     // Some decalrations
-        // Iterate over different matrix sizes
+        // 3 case unit , medium , large
+        double total_time[3] = {0.0, 0.0, 0.0};
+        for(int i = 0; i < 3; i++) {
+            vector<vector<double>> A;
+            vector<vector<double>> B;
+            if(i == 0) {
+                A = read_matrix_from_csv("./test/test_case/unit_negative_matrix_1x1.csv");
+                B = read_matrix_from_csv("./test/test_case/unit_zero_matrix_1x1.csv");
+            }
+            else if(i == 1) {
+                A = read_matrix_from_csv("./test/test_case/medium_positive_matrix_256x256.csv");
+                B = read_matrix_from_csv("./test/test_case/medium_zero_matrix_256x256.csv");
+            }
+            else {
+                A = read_matrix_from_csv("./test/test_case/large_zero_matrix_2048x2048.csv");
+                B = read_matrix_from_csv("./test/test_case/large_zero_matrix_2048x2048.csv");
+            }
         auto start = chrono::high_resolution_clock::now();
-        // Body code + output results each case 
+        auto result = matrix_mult(A, B);
         auto end = chrono::high_resolution_clock::now();
-        
+        chrono::duration<double, std::milli> duration = end - start;
+        total_time[i] += duration.count();
+        }
+        cout << "Performance Results (in milliseconds):\n";
+        cout << "1. Unit Test (1x1): " << total_time[0] << " ms\n";
+        cout << "2. Medium Test (256x256): " << total_time[1] << " ms\n";
+        cout << "3. Large Test (2048x2048): " << total_time[2] << " ms\n";
         cout<<"Performance test completed \n";
     
 }
