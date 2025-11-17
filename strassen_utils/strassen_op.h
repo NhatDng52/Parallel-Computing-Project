14966ab29f02a517375d69395a0a2f008cc20beb
Dang Tran Minh Nhat <supernhat256@gmail.com>
2025-11-17 15:48:28 +0700

    change make file to remove duplicate import , declare helper function in strassen op , call more test in main

--- strassen_utils/strassen_op.h
+++ strassen_utils/strassen_op.h
@@ -1,5 +1,8 @@
-#include<vector>
-#include<iostream>
+#ifndef STRASSEN_OP_H
+#define STRASSEN_OP_H
+
+#include <vector>
+#include <iostream>
 
 using namespace std;
 using Matrix = vector<vector<double>>;
@@ -8,3 +11,14 @@ class IStrassenOp {
     public:
         virtual Matrix apply_strassen(const Matrix &A, const Matrix &B) = 0;
 };
+
+
+//Naive strassen utils
+Matrix strassen_recursive(const Matrix &A, const Matrix &B);
+Matrix padding(const Matrix &A);
+vector<Matrix> separate_mat(const Matrix &A);
+Matrix combine_mat(const Matrix &C11, const Matrix &C12, const Matrix &C21, const Matrix &C22);
+
+#endif // STRASSEN_OP_H
+Matrix mat_add(const Matrix &A, const Matrix &B);
+Matrix mat_sub(const Matrix &A, const Matrix &B);
