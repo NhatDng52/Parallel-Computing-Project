c53b4044a28a7294a0d66676d20ba5d07f5ebdf9
Dang Tran Minh Nhat <supernhat256@gmail.com>
2025-11-17 15:41:55 +0700

    implement uparallel algo

--- matmul_algorithms/matmul_naive.cpp
+++ matmul_algorithms/matmul_naive.cpp
@@ -1,10 +1,17 @@
 #include "matmul_naive.h"
 using namespace std;
 vector<vector<double>> matrix_mult_naive(const vector<vector<double>>& A, const vector<vector<double>>& B) {
-    /*
-        Implement code 
-    */
-   return vector<vector<double>>{};
+    vector<vector<double>> result(A.size(), vector<double>(B[0].size(), 0.0));
+    for (size_t i = 0; i < A.size(); ++i) {
+        for (size_t j = 0; j < B[0].size(); ++j) {
+            double sum = 0.0;
+            for (size_t k = 0; k < A[0].size(); ++k) {
+                sum += A[i][k] * B[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+   return result;
 }
 vector<vector<double>> matrix_mult_naive_OpenMP(const vector<vector<double>>& A, const vector<vector<double>>& B) {
     /*
--- matmul_algorithms/matmul_strassen.cpp
+++ matmul_algorithms/matmul_strassen.cpp
@@ -1,20 +1,40 @@
 #include "matmul_strassen.h"
 #include "strassen_utils/strassen_op.h"
-#include "strassen_utils/strassen_open_mp.cpp"
-
 using namespace std;
 vector<vector<double>> matrix_mult_strassen(const vector<vector<double>>& A, const vector<vector<double>>& B) {
-    /*
-        Implement code 
-    */
-   return vector<vector<double>>{};
+    int n = A.size();
+    int m = B[0].size();
+    int k = A[0].size();
+
+
+    if(k != B.size()) throw "dimension mismatch";
+
+
+    // padding  
+    vector<vector<double>> A2 = padding(A);
+    vector<vector<double>> B2 = padding(B);
+
+
+    // recursive Strassen on padded matrices
+    vector<vector<double>> C2 = strassen_recursive(A2, B2);
+
+
+    // remove padding
+    vector<vector<double>> C(n, vector<double>(m));
+    for(int i = 0; i < n; i++)
+    for(int j = 0; j < m; j++)
+    C[i][j] = C2[i][j];
+
+
+    return C;
+
 }
 
 vector<vector<double>> matrix_mult_strassen_OpenMP(const vector<vector<double>>& A, const vector<vector<double>>& B) {
-    IStrassenOp * strassenOpenMP = new StrassenOpenMPI();
-    vector<vector<double>> result = strassenOpenMP->apply_strassen(A, B);
-    delete strassenOpenMP;
-    return result;
+    // IStrassenOp * strassenOpenMP = new StrassenOpenMPI();
+    // vector<vector<double>> result = strassenOpenMP->apply_strassen(A, B);
+    // delete strassenOpenMP;
+    // return result;
 }
 
 vector<vector<double>> matrix_mult_strassen_OpenMPI(const vector<vector<double>>& A, const vector<vector<double>>& B) {
